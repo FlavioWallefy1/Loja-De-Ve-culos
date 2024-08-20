bd6d62efbb0e888a4b3d4e5c6ca54b7ab96f1dc3
Flavio Wallefy da Silva Oliveira <fwso@palmares.ifpe.edu.br>
2024-08-20 11:08:42 -0300

    Inicializando o RenavamValidator

---
+++ src/br/edu/ifpe/loja/util/IValidator.java
@@ -0,0 +1,5 @@
+package br.edu.ifpe.loja.util;
+
+public interface IValidator {
+    int RenavamValidator(int renavam);
+}
---
+++ src/br/edu/ifpe/loja/util/RenavamValidador.java
@@ -0,0 +1,15 @@
+package br.edu.ifpe.loja.util;
+
+import br.com.caelum.stella.validation.RenavamValidator;
+import br.com.caelum.stella.validation.InvalidStateException;
+
+public class RenavamValidador implements IValidator {
+
+	@Override
+	public int RenavamValidator(int renavam) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+
+}
